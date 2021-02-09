f0ebd014462cf11e4c29ceb4136d31e0ddd2620b
ziad9918@gmail.com <74044173+izzo114@users.noreply.github.com>
2021-02-09 23:14:43 +0200

    login design

---
+++ app/src/main/java/com/example/login2/Login.java
@@ -0,0 +1,17 @@
+package com.example.login2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+        
+    }
+}
