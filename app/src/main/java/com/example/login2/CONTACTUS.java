9201d8dac9bdfe3b6953f0b99fc76efb6d1d797d
abdo <72812313+amrommar@users.noreply.github.com>
2021-06-22 21:44:58 +0200

    contact us commit

---
+++ app/src/main/java/com/example/login2/CONTACTUS.java
@@ -0,0 +1,14 @@
+package com.example.login2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CONTACTUS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c_o_n_t_a_c_t_u_s);
+    }
+}
